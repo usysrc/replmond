69e522decffac1e9ba8a2b540836ac25dd979fcc
usysrc <usysrc@usysrc.dev>
2024-05-05 19:36:57 +0200

    added save and load

--- todo.lua
+++ todo.lua
@@ -1,4 +1,5 @@
 local todos = {}
+local dones = {}
 
 local todo = function(a)
     table.insert(todos, a)
@@ -10,9 +11,45 @@ local display = function(t)
     end
 end
 
+local done = function(a)
+    local idx = 0
+    for i, v in ipairs(todos) do
+        if v == a then idx = i end
+    end
+    if idx > 0 then
+        table.remove(todos, idx)
+        table.insert(dones, a)
+    end
+end
+
+local save = function()
+    local f = io.open("todo.txt", "w")
+    for i, v in ipairs(todos) do
+        f:write(v .. "\n")
+    end
+    f:close()
+    print("Saved")
+end
+
+local load = function()
+    local f = io.open("todo.txt", "r")
+    if f then
+        for line in f:lines() do
+            todo(line)
+        end
+        f:close()
+    end
+end
+
+load()
+
 return setmetatable({
     todo = todo,
-    display = display
+    display = display,
+    done = done,
+    dones = dones,
+    save = save,
+    load = load,
 }, {
     __call = function(self, a)
         return todo(a)
